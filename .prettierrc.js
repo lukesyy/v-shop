834ff7635a8fb784bf1543a4bcd868f7fc4a51a8
lijingang <hh2019@qq.com>
2022-04-19 14:21:53 +0800

    up 格式化规则

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,13 +6,15 @@ module.exports = {
   // 不使用缩进符，而使用空格
   useTabs: false,
   // 行尾需要有分号
-  semi: true,
+  semi: false,
   // 使用单引号
   singleQuote: true,
   // 对象的 key 仅在必要时用引号
   quoteProps: 'as-needed',
   // jsx 不使用单引号，而使用双引号
   jsxSingleQuote: false,
+  // 在jsx中把'>' 是否单独放一行
+  jsxBracketSameLine: false,
   // 末尾需要有逗号
   trailingComma: 'all',
   // 大括号内的首尾需要空格
@@ -20,7 +22,7 @@ module.exports = {
   // jsx 标签的反尖括号需要换行
   bracketSameLine: false,
   // 箭头函数，只有一个参数的时候，也需要括号
-  arrowParens: 'always',
+  arrowParens: 'avoid',
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
